5ba4f025af4def0ba1fadaae8ae671159d94d562
claudioccm <gigeer@gmail.com>
2014-11-02 01:05:48 -0400

    Working with Angular and JSON
    - One nested loop
    - JSON Object is now an array
    - Single Controller for the card generation

--- js/card-generator.js
+++ js/card-generator.js
@@ -1,6 +1,21 @@
+// Controler to loop the cards from the DataBase
+
+var cardsApp = angular.module('cardsApp', []);
+
+cardsApp.controller('cardsController', function ($scope) {
+  $scope.cards = cards;
+  
+});
+
+
+// I can try this later. Maybe a solution to keep the row name.
+// var obj = {a: 1, b: 2, c: 3};
+// Object.keys(obj)[0]; // "a"
+
+
 // JSON DataBase
-var cards = {
-"designers": [
+var cards = [
+[
   {	"title": "Claudio Mendonca",
 	  "subtitle": "Graphic Designer",
 	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
@@ -22,7 +37,7 @@ var cards = {
 	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
   }
 ],
-"restaurants": [
+[
 	{	"title": "Ramen Ya",
 	  "subtitle": "Japa",
 	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
@@ -39,12 +54,24 @@ var cards = {
 	  "subtitle": "Vegan",
 	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
   }
-]};
+],
+[
+	{	"title": "Ramen Ya",
+	  "subtitle": "Japa",
+	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
+  },
+	{	"title": "Burger King",
+	  "subtitle": "Tosqueira",
+	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
+  },
+	{	"title": "Omnivore",
+	  "subtitle": "Saudável",
+	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
+  },
+	{	"title": "Pantera Verde",
+	  "subtitle": "Vegan",
+	  "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil aperiam nesciunt a iusto sapiente distinctio natus laboriosam hic."
+  }
+]
+];
 
-var cardsApp = angular.module('cardsApp', []);
-
-cardsApp.controller('designersController', function ($scope) {
-  $scope.designers = 	cards.designers	});
-
-cardsApp.controller('restaurantsController', function ($scope) {
-  $scope.restaurants = 	cards.restaurants	});
